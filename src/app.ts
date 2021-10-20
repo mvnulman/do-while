982e5b3750a0c472a696f580e9a3102f3edc8785
Marcos Vinicius <mvsouza7@gmail.com>
2021-10-20 19:40:53 +0300

    creating the logic behind the bussiness application. using Insomnia to test the infos by POST method and getting the GitHub informations that we need for the application. added jsonwebtoken dependencies to handle this damand as well

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import "dotenv/config"
 import express from 'express';
 
+import { router } from "./routes";
+
 const app = express();
+app.use(express.json());
+
+app.use(router);
 
 app.get("/github", (request, response) => {
     response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
@@ -12,6 +17,6 @@ app.get("/signin/callback", (request, response) => {
 
     return response.json(code);
 
-})
+});
 
 app.listen(4000, () => console.log(`🚀 Server is running on PORT 4000`));
---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,55 @@
+import axios from "axios";
+
+//---------------------------------------------
+// Bussiness steps
+//---------------------------------------------
+
+// * Receive code(string)
+// * Recovery the access_toke on github
+// * Recovery user infos on GitHub
+// * Verify if the user exist on our database:
+// * if YES = create a new token
+// * if NO = create on the DB and add new token
+// * Return the token with the logged user
+
+
+interface IAccessTokenResponse {
+    access_token: string
+}
+
+interface IUserResponse {
+    avatar_url: string,
+    login: string,
+    id: number,
+    name: string
+}
+
+
+class AuthenticateUserService {
+    async execute(code: string) {
+        const url = "https://github.com/login/oauth/access_token"
+
+        const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
+            params: {
+                client_id: process.env.GITHUB_CLIENT_ID,
+                client_secret: process.env.GITHUB_CLIENT_SECRET,
+                code,
+            },
+            headers: {
+                "Accept": "application/json"
+            },
+        });
+
+        const response = await axios.get<IUserResponse>("https://api.github.com/user", {
+            headers: {
+                authorization: `Bearer ${accessTokenResponse.access_token}`
+            },
+        });
+
+
+        return response.data;
+
+    }
+}
+
+export { AuthenticateUserService }
